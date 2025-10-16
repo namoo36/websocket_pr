d48cd6fd446b0be4279782313dbf506d1eb46c8b
namoo36 <wldnrkwhr36@naver.com>
2025-10-16 12:55:03 +0900

    inital commit

---
+++ src/main/java/com/namoo36/websocket_pr/WebsocketPrApplication.java
@@ -0,0 +1,13 @@
+package com.namoo36.websocket_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namoo36/websocket_pr/WebsocketPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namoo36.websocket_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
